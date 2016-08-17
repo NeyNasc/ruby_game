71c3889e96fc8c58470fbf90152161e106483233
Claudiney Nascimento <claudineydelima@gmail.com>
2016-08-17 20:59:25 -0300

    melhorando o código com arrays

--- array.rb
+++ array.rb
@@ -1,7 +1,11 @@
 chutes = []
-chutes[0] = 50
-chutes[1] = 150
-chutes[2] = 300
-tentativa = 3
+chutes<<50
+chutes<<100
+chutes<<300
 
-puts chutes
+puts chutes.size
+
+=begin
+for chute in chutes
+	puts chute
+=end
--- maior_ou_menor.rb
+++ maior_ou_menor.rb
@@ -2,30 +2,30 @@
 def da_boas_vindas
 	puts "Bem vindo ao jogo da adivinhação"
 	puts "Qual o seu nome?"
-	nome = gets
+	nome = gets.strip
 	puts "\n\n\n\n"
-	puts "Iremos começar o jogo para você, "+nome
+	puts "Iremos começar o jogo para você, #{nome}"
 end
 
 def sorteia_numero_secreto
 	puts "Escolhendo um número secreto entre 0 e 200..."
 	sorteado = 175
 	puts "Escolhido! que tal adivinhar hoje nosso número secreto?"
-	sorteado
+	sorteado.to_i
 end
 
 def pede_um_numero (chutes, tentativa, limite_de_tentativas)
 	puts "\n\n\n\n"
-	puts "Tentativa "+ tentativa.to_s+" de "+limite_de_tentativas.to_s
-	puts "Chutes até agora: "+chutes.to_s
+	puts "Tentativa #{tentativa} de #{limite_de_tentativas}"
+	puts "Chutes até agora: #{chutes}"
 	puts "Entre com o número"
-	chute = gets
-	puts "Será que acertou? Você chutou "+chute
+	chute = gets.strip
+	puts "Será que acertou? Você chutou #{chute}"
 	chute.to_i
 end
 
 def verifica_se_acertou(numero_secreto,chute)
-	acertou = $numero_secreto == chute
+	acertou = numero_secreto == chute
 
 	if acertou
 		puts "Acertou!"
@@ -41,27 +41,16 @@ def verifica_se_acertou(numero_secreto,chute)
 	end
 end
 
-
-
-
-
-
-
 #Invocações principais
 da_boas_vindas
 numero_secreto = sorteia_numero_secreto
-
 limite_de_tentativas = 5
 chutes =[]
 total_de_chutes = 0
 
+
 for tentativa in 1..limite_de_tentativas
 	chute = pede_um_numero chutes, tentativa, limite_de_tentativas
-
-	chutes[total_de_chutes] = chute
-	total_de_chutes+=1
-
-	puts "teste"
-
+	chutes << chute
 	break if verifica_se_acertou numero_secreto, chute	
 end
