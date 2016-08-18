be5f9bb2fb722db96ee5d99afe840732f6dc800e
Claudiney Nascimento <claudineydelima@gmail.com>
2016-08-18 12:20:22 -0300

    Adicionando a trapaça, escolha de nível e corrigindo o bug do sorteio numerérico

--- maior_ou_menor.rb
+++ maior_ou_menor.rb
@@ -5,11 +5,28 @@ def da_boas_vindas
 	nome = gets.strip
 	puts "\n\n\n\n"
 	puts "Iremos começar o jogo para você, #{nome}"
+	nome
 end
 
-def sorteia_numero_secreto
-	puts "Escolhendo um número secreto entre 0 e 200..."
-	sorteado = rand(200)
+def sorteia_numero_secreto(dificuldade)
+
+	case dificuldade
+	when 1
+		maximo = 30
+	when 2
+		maximo = 60
+	when 3
+		maximo = 100
+	when 4
+		maximo = 150
+	else
+		maximo = 200
+	end
+
+
+	maximo
+	puts "Escolhendo um número secreto entre 0 e #{maximo}..."
+	sorteado = rand(maximo)+1
 	puts "Escolhido! que tal adivinhar hoje nosso número secreto?"
 	sorteado.to_i
 end
@@ -41,9 +58,16 @@ def verifica_se_acertou(numero_secreto,chute)
 	end
 end
 
+def pede_dificuldade
+	puts "Qual o nível de dificuldade que deseja? (1 fácil, 5 difícil)"
+	dificuldade = gets.to_i
+end
+
+
+
 #Invocações principais
-da_boas_vindas
-numero_secreto = sorteia_numero_secreto
+nome = da_boas_vindas
+numero_secreto = sorteia_numero_secreto pede_dificuldade
 pontos_ate_asgora = 1000
 
 limite_de_tentativas = 5
@@ -54,6 +78,13 @@ total_de_chutes = 0
 for tentativa in 1..limite_de_tentativas
 	chute = pede_um_numero chutes, tentativa, limite_de_tentativas
 	chutes << chute
+
+	if nome == "Claudiney"
+		puts "Acertou!"
+		break
+	end
+
+
 	pontos_a_perder = (chute - numero_secreto).abs/2.0
 
 	pontos_ate_asgora -= pontos_a_perder
